61ee670905ac82343b635a998b1f12366d1cab4a
Maxim Kolotushkin <m.kolotushkin>
2021-08-03 18:27:14 +0300

    date formatted

--- src/utils/mapBondsToStorage.js
+++ src/utils/mapBondsToStorage.js
@@ -1,4 +1,5 @@
 const { differenceInCalendarDays } = require('date-fns');
+const { format } = require('date-fns');
 
 const daysTillRefund = (bond) => differenceInCalendarDays(new Date(bond[13]), new Date());
 const calculateCoupons = (bond) => Math.floor(daysTillRefund(bond) / bond[15]) + 1;
@@ -6,6 +7,8 @@ const calculateCoupons = (bond) => Math.floor(daysTillRefund(bond) / bond[15]) +
 const mapBondsToStorage = (rawResponse) => {
   const bonds = rawResponse?.securities?.data;
   const marketData = rawResponse?.marketdata?.data;
+  let matDate;
+  let nextCoupon;
   return bonds
     .map((bond) => {
       const fullPrice = (((+bond[8] * +bond[10]) / 100) + +bond[7]) * 1.00062;
@@ -15,14 +18,25 @@ const mapBondsToStorage = (rawResponse) => {
       const daysTillEnd = daysTillRefund(bond);
       const yearProfit = (percentProfit * 365) / daysTillEnd;
       const duration = marketData.find((data) => data[0] === bond[0])[36];
+      try {
+        matDate = format(new Date(bond[13]), 'dd.MM.yyyy');
+      } catch (err) {
+        matDate = 0;
+      }
+      try {
+        nextCoupon = format(new Date(bond[13]), 'dd.MM.yyyy');
+      } catch (err) {
+        nextCoupon = 0;
+      }
+
       return {
         key: bond[0],
         title: bond[2],
         price: bond[3],
         couponValue: bond[5],
-        nextCoupon: new Date(bond[6]),
+        nextCoupon,
         nkd: bond[7],
-        matDate: new Date(bond[13]),
+        matDate,
         fullPrice,
         couponInterval: bond[15],
         couponsLeft,
